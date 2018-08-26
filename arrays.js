17b0e090aba65ba75f90d951e99043f444ddcb9f
David masterson <david73180@gmail.com>
2018-08-26 22:34:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
   return array = [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift('')
+  return array.unshift(element)
 }
 function addElementToEndOfArray(array, element) {
   return array = [array, ...element]
